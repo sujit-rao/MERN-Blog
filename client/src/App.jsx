7f0efa65f0885edc6357cb6e4180e25d3423984c
Sujit Rao <sujitrao867@gmail.com>
2024-02-11 14:50:17 +0530

    Created the responsive header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Dashboard from './pages/Dashboard'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Home from './pages/Home'
+import Projects from './pages/Projects'
 import Header from './components/Header'
 
 
@@ -23,6 +24,7 @@ function App() {
 <Route path ="/sign-in" element={<SignIn />} />
 <Route path ="/sign-up" element={<SignUp />} />
 <Route path ="/dashboard" element={<Dashboard />} />
+<Route path = "/projects" element= {<Projects />} />
 
  </Routes>
  </BrowserRouter>
