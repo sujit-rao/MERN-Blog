b8c0861daa666119c63ba25b81d3055bd041807c
Sujit Rao <sujitrao867@gmail.com>
2024-02-12 12:13:13 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async(req,res,next) =>{
     const {username, email, password} = req.body;
@@ -25,4 +26,33 @@ export const signup = async(req,res,next) =>{
    catch (error){
     next(error);
    }
+}
+
+export const signin = async (req, res, next) => {
+    const {email, password} =req.body;
+    if( !email || !password || email === "" || password === ""){
+        next(errorHandler(400, 'All fields are required'))
+    }
+    try {
+        const validUser= await User.findOne({email})
+        if(!validUser){
+           return next(errorHandler(404, 'User not found'))
+        }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+        if(!validPassword) {
+          return next(errorHandler(400,'Invalid Password'))
+        }
+
+        const token = jwt.sign(
+        {id: validUser._id}, process.env.JWT_SECRET, {/*I can add expiration here as if browser is closed gotta sign in again (one-time session) with "{expiresIn: 'Id'}" */}
+        )
+const {password: pass, ...rest} = validUser._doc;
+
+            res.status(200).cookie('access_token', token,{
+                httpOnly: true
+            }).json(rest);
+    } catch (error) {
+        next(error)
+    }
 }
